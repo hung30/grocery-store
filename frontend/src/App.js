2a59e5c32f5fe556a69b1ee8c28ba215f4b10e45
hung30 <95275338+hung30@users.noreply.github.com>
2024-11-13 00:03:15 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import AdminLayout from "./layouts/AdminLayout";
 import { LoadingContext, LoadingProvider } from "./contexts/LoadingContext";
 import { useContext } from "react";
 import Spinner from "./components/spinner/Spinner";
-import ForgotPasswordPage from "./pages/authpage/ForgotPasswordPage";
+// import ForgotPasswordPage from "./pages/authpage/ForgotPasswordPage";
 
 const ProtectedRoutes = () => {
   const user = JSON.parse(localStorage.getItem("userInfo"));
@@ -70,7 +70,7 @@ function App() {
             <Route element={<UnauthorizedRoutes />}>
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              // <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             </Route>
 
             <Route element={<ProtectedRoutes />}>
